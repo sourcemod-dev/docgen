187673ea7bdfcd41d751013878c007329a9723f1
rumblefrog <contact@zhenyangli.me>
2019-12-26 00:03:38 -0500

    cleanup(generator::bindings) - remove old wasm attribute

--- generator/binding/docparse.h
+++ generator/binding/docparse.h
@@ -1,8 +1,6 @@
 #ifndef _DOCPARSE_H
 #define _DOCPARSE_H
 
-#define WASM_EXPORT __attribute__ ((visibility("default")))
-
-WASM_EXPORT const char* parse(const char* input, const char* path);
+const char* parse(const char* input, const char* path);
 
 #endif
