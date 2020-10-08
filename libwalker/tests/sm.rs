3493771e7b6f0e834386e24a300e181d58691c51
rumblefrog <contact@zhenyangli.me>
2020-10-07 23:05:46 -0400

    feat(walker) - track rev count (#9)
    * feat(walker) - track rev count

--- libwalker/tests/sm.rs
+++ libwalker/tests/sm.rs
@@ -14,6 +14,8 @@ fn test_walk() -> Result<(), Box<dyn std::error::Error>> {
 
     for t in spec_diffs {
         for c in t {
+            println!("{} {}", c.commit, c.count);
+
             std::fs::write(
                 format!("tests/contents/{}", c.commit.to_string()),
                 c.content,
