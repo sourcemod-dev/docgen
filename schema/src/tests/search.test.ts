e0bef44ea63e00b913fc8648005bc0e6cae504bb
rumblefrog <contact@zhenyangli.me>
2022-01-07 01:28:59 -0500

    fix(schema) - dist output

--- schema/src/tests/search.test.ts
+++ schema/src/tests/search.test.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Bundle, IBundle } from '../src';
+import { Bundle, IBundle, SearchResult } from '..';
 
 test('test', async () => {
     const data: IBundle = (await axios.get('https://raw.githubusercontent.com/sourcemod-dev/manifest/bundles/core.bundle'))
@@ -15,7 +15,7 @@ test('test', async () => {
 
     const ret = (await bundle.search('ArrayList', { parents: ['test_file'] }))
         .filter(e => e.score > 0.5)
-        .sort((a, b) => b.score - a.score);
+        .sort((a: SearchResult, b: SearchResult) => b.score - a.score);
 
     console.timeEnd('Searching bundle');
 
