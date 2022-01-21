3e2754916faa417926bc47bc1fa91c65552e8618
rumblefrog <contact@zhenyangli.me>
2022-01-21 01:33:26 -0500

    test(schema): update bundle path to new manifest

--- schema/src/tests/search.test.ts
+++ schema/src/tests/search.test.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Bundle, IBundle, SearchResult } from '..';
 
 test('test', async () => {
-    const data: IBundle = (await axios.get('https://gist.githubusercontent.com/rumblefrog/e1b25177b8d4df230bf1fd61dc8e71bf/raw/e3cf9ae618082f78179f80ddb9e9b5d0d22ffa50/t.bundle'))
+    const data: IBundle = (await axios.get('https://raw.githubusercontent.com/sourcemod-dev/manifest/bundles/core.bundle'))
         .data;
 
     console.time('Parsing bundle');
@@ -18,9 +18,9 @@ test('test', async () => {
 
     console.timeEnd('Searching bundle');
 
+    console.log('Looking up symbol by path:', ret[1].path);
+
     const l = bundle.getSymbolByPath(ret[1].path);
 
     console.log(l);
-
-    // console.log(ret);
 })
