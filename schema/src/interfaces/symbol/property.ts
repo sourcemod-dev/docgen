ca8b22569901f214db4a62fe4e0d79534bf081a1
rumblefrog <contact@zhenyangli.me>
2022-01-21 02:23:27 -0500

    feat(schema): class symbols type

--- schema/src/interfaces/symbol/property.ts
+++ schema/src/interfaces/symbol/property.ts
@@ -5,17 +5,17 @@ export interface IProperty extends IDeclaration {
      * @brief Type of the property
      * @readonly
      */
-    readonly type: string,
+    readonly type: string;
 
     /**
      * @brief Whether getter exists
      * @readonly
      */
-    readonly getter: boolean,
+    readonly getter: boolean;
 
     /**
      * @brief Whether setter exists
      * @readonly
      */
-    readonly setter: boolean,
+    readonly setter: boolean;
 }
